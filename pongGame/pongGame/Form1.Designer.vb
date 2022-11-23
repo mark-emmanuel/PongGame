152f454000ed536bca5742dacaf7c8b1b2943f8d
Mark <100807187+mark-emmanuel@users.noreply.github.com>
2022-11-23 17:11:01 -0400

    Game File Upload

---
+++ pongGame/pongGame/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Ball = New System.Windows.Forms.PictureBox()
+        Me.LeftPaddle = New System.Windows.Forms.PictureBox()
+        Me.RightPaddle = New System.Windows.Forms.PictureBox()
+        Me.LeftPaddleTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.RightPaddleTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.BallTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.LeftPlayerScore = New System.Windows.Forms.Label()
+        Me.RightPlayerScore = New System.Windows.Forms.Label()
+        Me.StartGameLabel = New System.Windows.Forms.Label()
+        Me.flashOnTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.flashOffTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.scoreTitle = New System.Windows.Forms.Label()
+        CType(Me.Ball, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LeftPaddle, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RightPaddle, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Ball
+        '
+        Me.Ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Ball.Image = CType(resources.GetObject("Ball.Image"), System.Drawing.Image)
+        Me.Ball.Location = New System.Drawing.Point(311, 174)
+        Me.Ball.Name = "Ball"
+        Me.Ball.Size = New System.Drawing.Size(50, 50)
+        Me.Ball.TabIndex = 0
+        Me.Ball.TabStop = False
+        '
+        'LeftPaddle
+        '
+        Me.LeftPaddle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.LeftPaddle.Image = CType(resources.GetObject("LeftPaddle.Image"), System.Drawing.Image)
+        Me.LeftPaddle.Location = New System.Drawing.Point(12, 127)
+        Me.LeftPaddle.Name = "LeftPaddle"
+        Me.LeftPaddle.Size = New System.Drawing.Size(30, 100)
+        Me.LeftPaddle.TabIndex = 1
+        Me.LeftPaddle.TabStop = False
+        '
+        'RightPaddle
+        '
+        Me.RightPaddle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.RightPaddle.Image = CType(resources.GetObject("RightPaddle.Image"), System.Drawing.Image)
+        Me.RightPaddle.Location = New System.Drawing.Point(742, 141)
+        Me.RightPaddle.Name = "RightPaddle"
+        Me.RightPaddle.Size = New System.Drawing.Size(30, 100)
+        Me.RightPaddle.TabIndex = 2
+        Me.RightPaddle.TabStop = False
+        '
+        'LeftPaddleTimer
+        '
+        Me.LeftPaddleTimer.Interval = 1
+        '
+        'RightPaddleTimer
+        '
+        Me.RightPaddleTimer.Interval = 1
+        '
+        'BallTimer
+        '
+        Me.BallTimer.Interval = 1
+        '
+        'LeftPlayerScore
+        '
+        Me.LeftPlayerScore.AutoSize = True
+        Me.LeftPlayerScore.Font = New System.Drawing.Font("Showcard Gothic", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.LeftPlayerScore.ForeColor = System.Drawing.Color.White
+        Me.LeftPlayerScore.Location = New System.Drawing.Point(334, 322)
+        Me.LeftPlayerScore.Name = "LeftPlayerScore"
+        Me.LeftPlayerScore.Size = New System.Drawing.Size(27, 30)
+        Me.LeftPlayerScore.TabIndex = 3
+        Me.LeftPlayerScore.Text = "0"
+        '
+        'RightPlayerScore
+        '
+        Me.RightPlayerScore.AutoSize = True
+        Me.RightPlayerScore.Font = New System.Drawing.Font("Showcard Gothic", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.RightPlayerScore.ForeColor = System.Drawing.Color.White
+        Me.RightPlayerScore.Location = New System.Drawing.Point(414, 322)
+        Me.RightPlayerScore.Name = "RightPlayerScore"
+        Me.RightPlayerScore.Size = New System.Drawing.Size(27, 30)
+        Me.RightPlayerScore.TabIndex = 4
+        Me.RightPlayerScore.Text = "0"
+        '
+        'StartGameLabel
+        '
+        Me.StartGameLabel.AutoSize = True
+        Me.StartGameLabel.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.StartGameLabel.Font = New System.Drawing.Font("Showcard Gothic", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.StartGameLabel.ForeColor = System.Drawing.Color.Red
+        Me.StartGameLabel.Location = New System.Drawing.Point(236, 9)
+        Me.StartGameLabel.Name = "StartGameLabel"
+        Me.StartGameLabel.Size = New System.Drawing.Size(295, 30)
+        Me.StartGameLabel.TabIndex = 5
+        Me.StartGameLabel.Text = "PRESS SPACE TO START"
+        '
+        'flashOnTimer
+        '
+        Me.flashOnTimer.Enabled = True
+        Me.flashOnTimer.Interval = 1000
+        '
+        'flashOffTimer
+        '
+        Me.flashOffTimer.Interval = 1000
+        '
+        'scoreTitle
+        '
+        Me.scoreTitle.AutoSize = True
+        Me.scoreTitle.Font = New System.Drawing.Font("Showcard Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.scoreTitle.ForeColor = System.Drawing.Color.White
+        Me.scoreTitle.Location = New System.Drawing.Point(367, 334)
+        Me.scoreTitle.Name = "scoreTitle"
+        Me.scoreTitle.Size = New System.Drawing.Size(45, 15)
+        Me.scoreTitle.TabIndex = 6
+        Me.scoreTitle.Text = "SCORE"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(784, 361)
+        Me.Controls.Add(Me.scoreTitle)
+        Me.Controls.Add(Me.StartGameLabel)
+        Me.Controls.Add(Me.RightPlayerScore)
+        Me.Controls.Add(Me.LeftPlayerScore)
+        Me.Controls.Add(Me.RightPaddle)
+        Me.Controls.Add(Me.LeftPaddle)
+        Me.Controls.Add(Me.Ball)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Ball, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LeftPaddle, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RightPaddle, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Ball As PictureBox
+    Friend WithEvents LeftPaddle As PictureBox
+    Friend WithEvents RightPaddle As PictureBox
+    Friend WithEvents LeftPaddleTimer As Timer
+    Friend WithEvents RightPaddleTimer As Timer
+    Friend WithEvents BallTimer As Timer
+    Friend WithEvents LeftPlayerScore As Label
+    Friend WithEvents RightPlayerScore As Label
+    Friend WithEvents StartGameLabel As Label
+    Friend WithEvents flashOnTimer As Timer
+    Friend WithEvents flashOffTimer As Timer
+    Friend WithEvents scoreTitle As Label
+End Class
---
+++ pongGame/pongGame/Form1.vb
@@ -0,0 +1,284 @@
+﻿''' <summary>
+''' Author: Mark Emmanuel A00388114
+''' Author: purpleshirtednerd - Followed youtube tutorial as base.
+''' 
+''' the user can play using the Q and A keys.
+''' the ball should speed up after each hit to make it 
+''' more challenging. 
+''' </summary>
+Public Class Form1
+
+    Dim BallTopSpeed, BallLeftSpeed, RPaddleSpeed, LPaddleSpeed As Decimal
+
+    ''' <summary>
+    ''' This allows the user to control the move the paddle 
+    ''' using the up and down arrow keys for the right paddle.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Randomize()
+        Dim X As Decimal
+        '''RIGHT PADDLE MOVEMENT
+        If e.KeyCode = Keys.Q Then   ' Keypress for the right paddle to move up
+            If RightPaddle.Top > 2 Then  ' Check to see if paddle has room to move up
+                RightPaddleTimer.Enabled = True
+                RPaddleSpeed = -Math.Abs(RPaddleSpeed)  'Set paddle speed to be negative
+            End If
+        End If
+
+        If e.KeyCode = Keys.A Then  ' Keypress for the right paddle to move down
+            If RightPaddle.Top + RightPaddle.Height < Me.Height - 20 Then  ' Check to see if paddle has room to move down
+                RightPaddleTimer.Enabled = True
+                RPaddleSpeed = Math.Abs(RPaddleSpeed)  'Set paddle speed to be positive
+            End If
+        End If
+
+
+        ''' GENERAL BALL MOVEMENT
+        If e.KeyCode = Keys.Space Then ' Space is used to start the game
+            StartGameLabel.Text = "" ' Start game text disappears when game starts
+            If BallTimer.Enabled = False Then
+                Randomize()
+                X = Rnd()  ' x will be decimal from .001 to .999
+                If X < 0.5 Then  '  Serve up or down... 50/50
+                    BallTopSpeed = -1 * BallTopSpeed ' Dirction change for ball
+
+                End If
+                X = Rnd()  ' x will be decimal from .001 to .999
+                If X < 0.5 Then  '  Serve right or left... 50/50
+                    BallLeftSpeed = -1 * BallTopSpeed ' Direction change for ball
+                End If
+                BallTimer.Enabled = True
+            End If
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' This stops the right paddle from leaving the screen
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub RightPaddleTimer_Tick(sender As Object, e As EventArgs) Handles RightPaddleTimer.Tick
+        RightPaddle.Top += RPaddleSpeed  'Move the paddle up or down
+
+        If RPaddleSpeed < 0 Then 'if the paddle is going up
+            If RightPaddle.Top < 2 Then  ' is the top of the paddle near the top of the form?
+                RightPaddleTimer.Enabled = False  ' if so don't let it run off the top of the form
+            Else
+                RightPaddle.Top += RPaddleSpeed  'Move the paddle up
+            End If
+        End If
+
+        If RPaddleSpeed > 0 Then 'if the paddle is going down
+            If RightPaddle.Top + RightPaddle.Height > Me.Height - 40 Then  ' is the bottom of the paddle near the bottom of hte form?
+                RightPaddleTimer.Enabled = False  ' if so don't let it run off the bottom of the form
+            Else
+                RightPaddle.Top += RPaddleSpeed  'Move the paddle down
+            End If
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' This stops the left paddle from leaving the screen
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub LeftPaddleTimer_Tick(sender As Object, e As EventArgs) Handles LeftPaddleTimer.Tick
+
+        LeftPaddle.Top += LPaddleSpeed
+        If LPaddleSpeed < 0 Then 'if the paddle is going up
+            If LeftPaddle.Top < 2 Then  ' is the top of the paddle near the top of the form?
+                LeftPaddleTimer.Enabled = False  ' if so don't let it run off the top of the form
+            Else
+                LeftPaddle.Top += LPaddleSpeed  'Move the paddle up
+            End If
+        End If
+
+        If LPaddleSpeed > 0 Then 'if the paddle is going down
+            If LeftPaddle.Top + RightPaddle.Height > Me.Height - 40 Then ' is the bottom of the paddle near the bottom of hte form?
+                LeftPaddleTimer.Enabled = False  ' if so don't let it run off the bottom of the form
+            Else
+                LeftPaddle.Top += LPaddleSpeed  'Move the paddle up
+            End If
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Flashing on text for start screen.
+    ''' This is done by linking the timers to the label
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub flashOnTimer_Tick(sender As Object, e As EventArgs) Handles flashOnTimer.Tick
+        StartGameLabel.Visible = True
+        flashOnTimer.Enabled = False
+        flashOffTimer.Enabled = True
+    End Sub
+
+    ''' <summary>
+    ''' Flashing off text for start screen
+    ''' This is done by linking the timers to the label
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub flashOffTimer_Tick(sender As Object, e As EventArgs) Handles flashOffTimer.Tick
+        StartGameLabel.Visible = False
+        flashOnTimer.Enabled = True
+        flashOffTimer.Enabled = False
+    End Sub
+
+    ''' <summary>
+    ''' Checks for the ball hitting either paddle.
+    ''' Checks for the ball hitting the top/bottom of the screen.
+    ''' Method for when ball goes past a paddle.
+    ''' Computer control for the left paddle.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub BallTimer_Tick(sender As Object, e As EventArgs) Handles BallTimer.Tick
+        Dim gameOver
+        '''COMPUTER CONTROL FOR THE LEFT PADDLE
+        If LeftPaddle.Top > 2 Then  ' Check to see if paddle has room to move up
+            If Ball.Top <= LeftPaddle.Top Then
+                LeftPaddleTimer.Enabled = True
+                LPaddleSpeed = -Math.Abs(LPaddleSpeed)  'Set paddle speed to be negative
+            End If
+        End If
+
+        If LeftPaddle.Top + LeftPaddle.Height < Me.Height - 20 Then  ' Check to see if paddle has room to move down
+            If Ball.Top >= LeftPaddle.Top Then
+                LeftPaddleTimer.Enabled = True
+                LPaddleSpeed = Math.Abs(LPaddleSpeed)  'Set paddle speed to be positive
+            End If
+        End If
+
+        Ball.Top += BallTopSpeed   'Move the ball up or down
+        Ball.Left += BallLeftSpeed  'Move the ball left or right
+
+
+
+
+        '''CHECKS FOR THE BALL HITTING THE TOP/BOTTOM OF THE SCREEN
+        'Hits the top
+        If Ball.Top < 2 Then  'if the ball is near the top of the form
+            BallTopSpeed = -1 * BallTopSpeed 'bounce off the top (or send the ball down)
+        End If
+
+        'hits the bottem
+        If Ball.Top > Me.Height - 80 Then  'if the ball is near the bottom of the form
+            BallTopSpeed = -1 * BallTopSpeed  'bounce off the bottom (or send the ball up)
+
+        End If
+
+
+        '''TESTS IF THE BALL HITS THE RIGHT PADDLE
+        If Math.Abs(Ball.Right - RightPaddle.Left) < BallLeftSpeed / 2 + 1 Then
+            If Ball.Top < RightPaddle.Bottom Then
+                If Ball.Bottom > RightPaddle.Top Then
+                    If BallTopSpeed < 8 Then ' Max speed set to 8
+                        BallLeftSpeed = -1 * (BallLeftSpeed * 1.2) ' Speeds the ball up after direction change
+                        BallTopSpeed = 1.2 * BallTopSpeed
+                    Else
+                        BallLeftSpeed = -1 * BallLeftSpeed ' Once ball reaches max speed, keeps it constant
+                        BallTopSpeed = BallTopSpeed
+                    End If
+                End If
+            End If
+        End If
+
+        '''TESTS IF THE BALL HITS THE LEFT PADDLE
+        If Math.Abs(Ball.Left - LeftPaddle.Right) < Math.Abs(BallLeftSpeed / 2) + 1 Then
+            If Ball.Top < LeftPaddle.Bottom Then
+                If Ball.Bottom > LeftPaddle.Top Then
+                    If BallTopSpeed < 8 Then ' Max speed set to 8
+                        BallLeftSpeed = -1 * (BallLeftSpeed * 1.2) ' Speeds the ball up after direction change
+                        BallTopSpeed = 1.2 * BallTopSpeed
+                    Else
+                        BallLeftSpeed = -1 * BallLeftSpeed ' Once ball reaches max speed, keeps it constant
+                        BallTopSpeed = BallTopSpeed
+                    End If
+                End If
+            End If
+        End If
+
+        ''' METHOD FOR WHEN BALL GOES PAST A PADDLE
+        'Check for when ball goes past right paddle
+        If Ball.Left > Me.Width Then   ' If ball went off the right side
+            BallTimer.Enabled = False  ' Stop this timer
+            Ball.Left = 0.5 * Me.Width - 20  ' Place the ball halfway through the width of the screen
+            Ball.Top = 0.5 * Me.Height - 40  ' Place the ball halfway through the height Of the screen
+            LeftPlayerScore.Text = 1 + Val(LeftPlayerScore.Text) ' Score for left player
+            StartGameLabel.Text = "PRESS SPACE TO START" ' Start game text
+            BallTopSpeed = 4 ' Resets ball speed to 4 when it goes off screen
+            BallLeftSpeed = 4
+            If Val(LeftPlayerScore.Text) = 5 Then ' Score limit
+                StartGameLabel.Text = "Computer Won!"
+                My.Computer.Audio.Play(My.Resources.sound, AudioPlayMode.WaitToComplete) ' Plays winning sound
+                gameOver = MsgBox("The Computer Won, would you like to retry?", MsgBoxStyle.YesNo)
+                If gameOver = 7 Then ' 7 is link to user clicking "no"
+                    End
+                Else
+                    LeftPlayerScore.Text = 0 ' Reset scores
+                    RightPlayerScore.Text = 0
+                End If
+            End If
+        End If
+
+        'Check for when ball goes past left paddle
+        If Ball.Left < 0 Then   ' If ball went off the left side
+            BallTimer.Enabled = False  ' Stop this timer
+            Ball.Left = 0.5 * Me.Width - 20  ' Place the ball halfway through the width of the screen
+            Ball.Top = 0.5 * Me.Height - 40  ' Place the ball halfway through the height Of the screen
+            RightPlayerScore.Text = 1 + Val(RightPlayerScore.Text) ' Score for right player
+            StartGameLabel.Text = "PRESS SPACE TO START" ' Start game text
+            BallTopSpeed = 4 ' Resets ball speed to 4 when it goes off screen
+            BallLeftSpeed = 4
+            If Val(RightPlayerScore.Text) = 5 Then ' Score limit
+                StartGameLabel.Text = "You Won!"
+                My.Computer.Audio.Play(My.Resources.sound, AudioPlayMode.WaitToComplete) ' Plays winning sound
+                gameOver = MsgBox("You Won, would you like to play again?", MsgBoxStyle.YesNo)
+                If gameOver = 7 Then ' 7 is link to user clicking "no"
+                    End
+                Else
+                    LeftPlayerScore.Text = 0 ' Reset scores
+                    RightPlayerScore.Text = 0
+                End If
+            End If
+        End If
+    End Sub
+
+
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        If e.KeyCode = Keys.Q Then  ' Stops right paddle upward movement
+            RightPaddleTimer.Enabled = False
+
+        End If
+        If e.KeyCode = Keys.A Then  ' Stops right paddle downward movement
+            RightPaddleTimer.Enabled = False
+
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' All intial values required to display correctly.
+    ''' Including inital ball/paddle speed.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LeftPaddle.BackColor = Color.Transparent
+        RightPaddle.BackColor = Color.Transparent
+        Ball.BackColor = Color.Transparent
+        StartGameLabel.BackColor = Color.Transparent
+        LeftPlayerScore.BackColor = Color.Transparent
+        RightPlayerScore.BackColor = Color.Transparent
+        scoreTitle.BackColor = Color.Transparent
+        BallTopSpeed = 4 '  Ball moves up or down in increments of 4 
+        BallLeftSpeed = 4 ' Ball moves left or right in increments of  4
+        RPaddleSpeed = 5  ' Right paddle moves in increments of 5
+        LPaddleSpeed = 5  ' Left paddle moves in increments of 5
+    End Sub
+End Class
